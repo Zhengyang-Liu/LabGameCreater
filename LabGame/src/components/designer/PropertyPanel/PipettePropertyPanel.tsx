b9400d5c296316447d7c7c43fb9544d691664ca7
Zhengyang Liu <liuzhengyang183@gmail.com>
2019-03-25 18:27:40 +0800

    Revert "add LiquidPanelComponent"
    This reverts commit 6e30c00836bc612149c7750db5181129f046aa90.

--- LabGame/src/components/designer/PropertyPanel/PipettePropertyPanel.tsx
+++ LabGame/src/components/designer/PropertyPanel/PipettePropertyPanel.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import * as Types from '../../../types';
-import LiquidPanelComponent from './sharedPanelComponent/LiquidPanelComponent';
+import { Control } from 'react-redux-form';
+import { FormGroup, Label } from 'reactstrap';
 
+import { LiquidList } from '../../../shared/LiquidList';
+import * as Types from '../../../types';
 
 interface Props {
     itemProperty: Types.PipetteDataProperty
@@ -11,9 +13,45 @@ class PipettePropertyPanel extends React.Component<Props> {
     constructor(props: Props) {
         super(props);
     }
+
+    handleLiquidTypeChange = (event) => {
+        this.props.itemProperty.liquidType = event.target.value;
+    }
+
+    handleVolumeChange = (event) => {
+        this.props.itemProperty.volume = parseInt(event.target.value);
+    }
+
     render = () => {
+        const LiquidTypes = LiquidList.map(item => {
+            return (
+                <option>{item}</option>
+            );
+        })
         return (
-            <LiquidPanelComponent liquidList={this.props.itemProperty.liquid} />
+            <>
+                <FormGroup>
+                    <Label>Liquid Type</Label>
+                    <Control.select
+                        model="selectedItem.property.liquidType"
+                        className="form-control"
+                        placeholder="Liquid Type"
+                        onChange={this.handleLiquidTypeChange}
+                    >
+                        {LiquidTypes}
+                    </Control.select>
+                </FormGroup>
+
+                <FormGroup>
+                    <Label>Volume</Label>
+                    <Control.text
+                        model="selectedItem.property.volume"
+                        className="form-control"
+                        placeholder="Volume"
+                        onChange={this.handleVolumeChange}
+                    ></Control.text>
+                </FormGroup>
+            </>
         )
     }
 }
