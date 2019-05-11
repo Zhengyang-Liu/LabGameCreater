4ac1153739e3f3134c1d8d4f151f45d37a63084f
Zhengyang Liu <liuzhengyang183@gmail.com>
2019-05-11 14:43:36 +0800

    nothing

---
+++ LabGame/src/mongodb/client.ts
@@ -0,0 +1,12 @@
+// export const MongoClient = require('mongodb').MongoClient;
+// //const assert = require('assert');
+// export const dboper = require('./operations');
+
+// export const url = 'mongodb://localhost:27017/';
+// export const dbname = 'LabGameCreater';
+
+// MongoClient.connect(url).then((client) => {
+
+//     console.log('Connected correctly to server');
+//     const db = client.db(dbname);
+// });
